590b82294c847755b59119937ac2c10d306aaaa2
tmango-lab <tmango.fb@gmail.com>
2026-01-14 10:20:44 +0700

    แก้ harcdoe ออก

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,8 +1,8 @@
 
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = "https://lvrvryneibnirjsphkud.supabase.co"
-const supabaseAnonKey = "sb_publishable_7z-BTM5F54165vsR5Lu83w_qm2s1Ndc"
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 if (!supabaseUrl || !supabaseAnonKey) {
     console.warn('Missing Supabase URL or Anon Key')
